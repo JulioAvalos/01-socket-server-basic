7baae0b9e35440a8e17e3d673baccef66723bbed
JulioAvalos <julioavalos2@msn.com>
2021-07-15 21:43:39 -0600

    connecting html page to our socket server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Servidor de express
-const app = require("express")();
+const express = require('express');
+const app = express();
 
 // Servidor de sockets 
 const server = require("http").createServer(app);
@@ -7,7 +8,12 @@ const server = require("http").createServer(app);
 // Configuracion del socket server
 const io = require("socket.io")(server);
 
-io.on("connection", () => {/* … */});
+// Desplegar el directorio publico
+app.use(express.static(__dirname + '/public'));
+
+io.on("connection", () => {
+    console.log('Cliente conectado!')
+});
 
 server.listen(8080, () => {
     console.log('Server corriendo en puerto :8080');
