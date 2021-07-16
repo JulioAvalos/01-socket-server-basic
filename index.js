256b8de7ca8c519fc2a9b6b3920b47076864898b
JulioAvalos <julioavalos2@msn.com>
2021-07-15 21:21:27 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Servidor de express
+const app = require("express")();
+
+// Servidor de sockets 
+const server = require("http").createServer(app);
+
+// Configuracion del socket server
+const io = require("socket.io")(server);
+
+io.on("connection", () => {/* … */});
+
+server.listen(8080, () => {
+    console.log('Server corriendo en puerto :8080');
+});
